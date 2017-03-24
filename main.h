f6ba7b993af4855f1801a0d12224744fb67643a9
Aliaksandr Sakharau <aleaksah@gmail.com>
2017-03-24 15:26:43 +0300

    GL context initialization

--- main.h
+++ main.h
@@ -1,10 +1,32 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+//pre-include definitions
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+//libraries
+#pragma comment (lib, "Opengl32.lib")
+
+//inclused
 #include <Windows.h>
 #include <stdio.h>
 #include <gl\GL.h>
 
+//definiions
+#define GL_TEST_CLASS_NAME (L"gl_test class")
+#define HEIGHT	(480)
+#define WIDTH	(640)
 
+//global variables
+HINSTANCE hInst = NULL;
+HWND hWnd;
+HDC ghDC;
+HGLRC ghRC;
 
+//function declarations
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+BOOL bSetupPixelFormat(HDC hdc);
+GLvoid drawScene();
 #endif
